e87b198366502fad5c9263dac20727f8e96103aa
liningrui <liningrui@baidu.com>
2018-12-24 15:39:40 +0800

    Add travis api-test
    Change-Id: I01d10dc092f2f2147155bc2b523b49e741d8d571

--- hugegraph-hbase/src/main/java/com/baidu/hugegraph/backend/store/hbase/HbaseSessions.java
+++ hugegraph-hbase/src/main/java/com/baidu/hugegraph/backend/store/hbase/HbaseSessions.java
@@ -21,9 +21,7 @@ package com.baidu.hugegraph.backend.store.hbase;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
